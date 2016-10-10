b191b08e3007d62e33a11968e3ab7214cbe39a94
YareArista <yarearista@gmail.com>
2016-10-10 16:28:51 -0500

    Segunda parte de la evaluacion

---
+++ segunda parte/ejercicio1.js
@@ -0,0 +1,9 @@
+var palabra = ["l","a","r","u","t","a","n","o","s","a","p","o","r","t","o","o","t","r","o","p","a","s","o","n","a","t","u","r","a","l"];
+
+var palindromo = palabra.reverse(palabra);
+
+if (palabra == palindromo) {
+	alert("Es un palindromo");
+}else{
+	alert("No es un palindromo");
+}
